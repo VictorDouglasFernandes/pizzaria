fedfe1e70f98f1f2c3cc349bb4b3d73ed84d9999
VictorDouglasFernandes <victordouglasfernandes@gmail.com>
2022-06-20 16:38:00 -0300

    Adicionar Sistema

--- controle/controle_sistema.py
+++ controle/controle_sistema.py
@@ -1,14 +1,51 @@
+from tela.tela_sistema import TelaSistema
+from entidade.funcionario import Funcionario
+from controle.controle_pizza import ControlePizza
+from controle.controle_cliente import ControleCliente
+from controle.controle_ingrediente import ControleIngrediente
+from controle.controle_pedido import ControlePedido
+from controle.controle_relatorio import ControleRelatorio
 
 
 class ControleSistema:
     def __init__(self):
-        pass
+        self.__tela_sistema = TelaSistema()
+        self.__funcionarios = [Funcionario("Funcionário 1", "1", "1")]
+        self.__controle_pizza = ControlePizza()
+        self.__controle_cliente = ControleCliente()
+        self.__controle_pedido = ControlePedido(self.__controle_pizza,
+                                                self.__controle_cliente)
+        self.__controle_ingrediente = ControleIngrediente()
+        self.__controle_relatorio = ControleRelatorio(self.__controle_pedido)
 
     def iniciar(self):
-        pass
+        self.__tela_sistema.bem_vindo()
+        self.logar()
 
     def logar(self):
-        pass
+        retorno = self.__tela_sistema.logar()
+        if retorno["cpf"] == "" and retorno["senha"] == "":
+            exit()
+        for funcionario in self.__funcionarios:
+            if funcionario.cpf == retorno["cpf"]:
+                if funcionario.senha == retorno["senha"]:
+                    self.__tela_sistema.mensagem("Bem vindo " +
+                                                 funcionario.nome)
+                    self.menu()
+        else:
+            self.__tela_sistema.mensagem("Usuário ou Senha incorreto")
+            self.logar()
 
     def menu(self):
-        pass
+        retorno = self.__tela_sistema.menu()
+        funcao_opcoes = {
+            0: self.logar,
+            1: self.__controle_pedido.menu,
+            2: self.__controle_pizza.menu,
+            3: self.__controle_cliente.menu,
+            4: self.__controle_ingrediente.menu,
+            5: self.__controle_relatorio.menu
+        }
+
+        funcao_opcoes[retorno]()
+        self.menu()
--- tela/tela_sistema.py
+++ tela/tela_sistema.py
@@ -6,16 +6,35 @@ class TelaSistema(Tela):
         pass
 
     def bem_vindo(self):
-        pass
+        print("Bem vindo ao Sistema Pizzaria\n")
 
     def menu(self):
-        pass
+        while True:
+            try:
+                print("==== Tela Sistema ====")
+                print("1 - Pedidos")
+                print("2 - Pizzas")
+                print("3 - Clientes")
+                print("4 - Ingredientes")
+                print("5 - Relatório")
+                self.retornar()
+                opcao = int(input())
+                if isinstance(opcao, int):
+                    if opcao == 0 or opcao == 1 or opcao == 2:
+                        return opcao
+                    if opcao == 3 or opcao == 4 or opcao == 5:
+                        return opcao
+            except:
+                pass
 
     def logar(self):
-        pass
+        print("==== Login ====")
+        cpf = input("cpf: ")
+        senha = input("senha: ")
+        return {"cpf": cpf, "senha": senha}
 
     def mensagem(self, mensagem: str):
-        pass
+        print(mensagem)
 
     def retornar(self):
-        pass
+        print("0 - Sair")
