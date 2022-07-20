36451d58e941a2eff73c61078553609ae2249746
GuilhermePrandii <101130409+GuilhermePrandii@users.noreply.github.com>
2022-07-20 10:24:52 -0300

    Parte UI Cliente: Alterar e correções

--- tela/tela_cliente.py
+++ tela/tela_cliente.py
@@ -1,48 +1,76 @@
 from tela.tela import Tela
-
+import PySimpleGUI as sg
 
 class TelaCliente(Tela):
     def __init__(self):
-        pass
+        self.__window = None
 
     def menu(self):
-        while True:
-            try:
-                print("==== Tela Cliente ====")
-                print("1 - Listar")
-                print("2 - Adicionar")
-                print("3 - Alterar")
-                print("4 - Excluir")
-                self.retornar()
-                opcao = int(input())
-                if isinstance(opcao, int):
-                    if opcao == 0 or opcao == 1 or opcao == 2:
-                        return opcao
-                    if opcao == 3 or opcao == 4:
-                        return opcao
-            except:
-                pass
-
+        layout = [
+            [sg.Text('Tela Cliente', font=("Helvica", 25))],
+            [sg.Text('Escolha sua opção', font=("Helvica", 15))],
+            [sg.Radio('Listar', "RD1", key='1')],
+            [sg.Radio('Adicionar', "RD1", key='2')],
+            [sg.Radio('Alterar', "RD1", key='3')],
+            [sg.Radio('Excluir', "RD1", key='4')],
+            [sg.Button('Confirmar'), sg.Cancel('Cancelar')]
+            ]
+        self.__window = sg.Window('Sistema Pizzaria').Layout(layout)
+        button, values = self.__window.Read()
+        opcao = 0
+        if values['1']:
+            opcao = 1
+        elif values['2']:
+            opcao = 2
+        elif values['3']:
+            opcao = 3
+        elif values['4']:
+            opcao = 4
+        if button in (None, 'Cancelar'):
+            opcao = 0
+        self.__window.Close()
+        return opcao
+    
     def adicionar(self):
-        print("==== Adicionar Cliente ====")
-        nome = input("Nome: ")
-        cpf = input("CPF: ")
-        endereco = input("Endereço: ")
-        return {"nome": nome, "cpf": cpf, "endereco": endereco}
+        layout = [
+            [sg.Text('Adicionar Cliente', font=("Helvica", 25))],
+            [sg.Text('Nome: '), sg.In(key='nome')],
+            [sg.Text('CPF: '), sg.In(key='cpf')],
+            [sg.Text('Endereço: '), sg.In(key='endereco')],
+            [sg.Button('Confirmar'), sg.Cancel('Cancelar')]
+        ]
+        self.__window = sg.Window('Sistema Pizzaria').Layout(layout)
+        button, values = self.__window.Read()
+        self.__window.close()
+        return {
+            "nome": values['nome'],
+            'cpf': values['cpf'],
+            'endereco': values['endereco']
+            }
 
     def pegar_cpf(self):
         return input("CPF: ")
 
     def alterar(self):
-        print("==== Alterar Cliente ====")
-        nome = input("Nome: ")
-        cpf = input("CPF: ")
-        endereco = input("Endereço: ")
-        return {"nome": nome, "cpf": cpf, "endereco": endereco}
-
+        layout = [
+            [sg.Text('Alterar Cliente', font=("Helvica", 25))],
+            [sg.Text('Nome: '), sg.In(key='nome')],
+            [sg.Text('CPF: '), sg.In(key='cpf')],
+            [sg.Text('Endereço: '), sg.In(key='endereco')],
+            [sg.Button('Confirmar'), sg.Cancel('Cancelar')]
+            ]
+        self.__window = sg.Window('Sistema Pizzaria').Layout(layout)
+        button, values = self.__window.Read()
+        self.__window.close()
+        return {
+            "nome": values['nome'],
+            'cpf': values['cpf'],
+            'endereco': values['endereco']
+            }
+        
     def mostrar_cliente(self, cliente):
       print("Nome:", cliente.nome)
       print("CPF: ", cliente.cpf, "\n")
 
     def mensagem(self, mensagem: str):
-        print(mensagem)
+        sg.popup('', mensagem)
